700f54f0acc515b8ad5c4cb28dd802a92940725f
Lewis Ames <lewis.ames@mybrightwheel.com>
2022-01-11 10:31:24 -0800

    replaces faker with local copy as og as been removed and forked project is not yet hosted on npm

--- tools/mockData.js
+++ tools/mockData.js
@@ -1,4 +1,4 @@
-const faker = require("faker");
+const faker = require("./faker");
 const { createAvatar } = require("@dicebear/avatars");
 const style = require("@dicebear/avatars-avataaars-sprites");
 
